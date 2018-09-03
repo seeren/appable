1e2c76a6653cfd92bbb90f51979f6c66742db220
Cyril <consultant@seeren.fr>
2018-09-03 23:29:44 +0200

    Add foo component

---
+++ src/app/foo/foo.component.js
@@ -0,0 +1,20 @@
+import { Component } from '../../../core/core.component';
+import foo from './foo.component.html';
+
+export class FooComponent extends Component {
+
+    constructor() {
+        super({
+            selector: 'foo',
+            template: foo
+        });
+
+        this.enable = false;
+
+    }
+
+    toogle() {
+        return this.enable = !this.enable;
+    }
+
+}
