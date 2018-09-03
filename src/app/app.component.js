033d40a99ae90d8d60db4330c679aa48c7e2a954
Cyril <consultant@seeren.fr>
2018-09-03 23:29:26 +0200

    Detach components

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -1,17 +1,13 @@
 import { Component } from '../../core/core.component';
 
 import { app } from './app.component.html';
-import { DemoComponent } from './demo/demo.component';
 
 export class AppComponent extends Component {
 
     constructor() {
         super({
             selector: "app",
-            template: app,
-            components: [
-                new DemoComponent
-            ]
+            template: app
         });
     }
 
