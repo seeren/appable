f7fb63d37b88142571b4ca38dd8cf5bf70357fa6
Cyril <consultant@seeren.fr>
2018-09-30 00:37:29 +0200

    Add demo service

---
+++ src/app/shared/services/materias.service.js
@@ -0,0 +1,50 @@
+import { Materia } from './../models/materia.model'
+import { Service } from '../../../../core/service';
+
+export const MateriasService = new class MateriasService extends Service {
+
+    constructor() {
+        super();
+        this.materia;
+        this.materias = [
+            new Materia(
+                1,
+                "Foo attaque",
+                "Description",
+                Materia.MAGIC,
+                4
+            ),
+            new Materia(
+                2,
+                "Bar attaque",
+                "Description",
+                Materia.MAGIC,
+                4
+            ),
+            new Materia(
+                3,
+                "Baz attaque",
+                "Description",
+                Materia.MAGIC,
+                4
+            )
+        ];
+    }
+
+    get() {
+        return this.materias
+    }
+
+    post(materia) {
+        this.materias.push(materia)
+    }
+
+    selected() {
+        return this.materia;
+    }
+
+    select(id) {
+        this.materia = this.materias.find(materia => id === materia.id)
+        this.notify();
+    }
+}
