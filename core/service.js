18d0833999326580f77b61b7e3d3fc1d8c96a617
Cyril <consultant@seeren.fr>
2018-10-01 15:17:09 +0200

    Add detach

--- core/service.js
+++ core/service.js
@@ -14,9 +14,21 @@ export class Service {
         let callback = [];
 
         /**
-         * @param {Function} callback
+         * @param {Function} callable
          */
         this.attach = callable => callback.push(callable);
+
+        /**
+         * @param {Function} callable 
+         */
+        this.detach = (callable) => {
+            for (let key in callback) {
+                if (callable === callback[key]) {
+                    callback.splice(key, 1);
+                    break;
+                }
+            }
+        }
 
         /**
          * @returns {void}
