dce822fb1001783f41541d77a3297f435ca9fa8c
Cyril <consultant@seeren.fr>
2018-10-02 12:37:07 +0200

    Remove const

--- src/app/shared/models/materia.model.js
+++ src/app/shared/models/materia.model.js
@@ -1,25 +1,5 @@
 export class Materia {
 
-    static MAGIC() {
-        return "Magic"
-    }
-
-    static SUPPORT() {
-        return "Support"
-    }
-
-    static ORDER() {
-        return "Order"
-    }
-
-    static INDEPENDENT() {
-        return "Independent"
-    }
-
-    static INVOCATION() {
-        return "Invocation"
-    }
-
     constructor(id, name, description, type, levels) {
         this.id = id;
         this.name = name;
