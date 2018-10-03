99a7a22904ddb1d2152926fb1e12c60c9d3d61e4
Cyril <consultant@seeren.fr>
2018-10-03 02:45:01 +0200

    Typo fix

--- src/app/shared/models/materia.model.js
+++ src/app/shared/models/materia.model.js
@@ -1,11 +1,11 @@
 export class Materia {
 
-    constructor(id, name, description, type, levels) {
+    constructor(id, name, description, type, level) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.type = type;
-        this.levels = levels
+        this.level = level
     }
 
 }
