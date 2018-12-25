9354d9153de0c58150031b14fcdaaeb6568ab5c9
seeren <consultant@seeren.fr>
2018-12-25 17:47:11 +0100

    Documentate

--- cli/file/file.js
+++ cli/file/file.js
@@ -3,6 +3,10 @@ const path = require("path");
 
 module.exports = {
 
+    /**
+     * @param {srting} filename 
+     * @param {srting} output 
+     */
     write(filename, output) {
         if (!this.createDirs(filename.split("/"))) {
             return false;
@@ -10,23 +14,33 @@ module.exports = {
         try {
             fs.writeFileSync(filename, output);
         } catch (e) {
-            console.error("\x1b[31m", "\u00D7", "\x1b[0m", `Generate error: ${e.message}`);
+            console.error(
+                "\x1b[31m", "\u00D7", "\x1b[0m", `Generate error: ${e.message}`
+            );
             return false;
         }
         console.log("\x1b[32m", "\u2713", "\x1b[0m", filename);
         return true;
     },
 
+    /**
+     * @param {string} filename 
+     */
     read(filename) {
         let output;
         try {
             output = fs.readFileSync(filename);
         } catch (e) {
-            console.error("\x1b[31m", "\u00D7", "\x1b[0m", `Read error: ${e.message}`);
+            console.error(
+                "\x1b[31m", "\u00D7", "\x1b[0m", `Read error: ${e.message}`
+            );
         }
         return output;
     },
 
+    /**
+     * @param {Array} dirs 
+     */
     createDirs(dirs) {
         let currentDir;
         for (let i = 1, l = dirs.length; i < l; i++) {
@@ -36,13 +50,18 @@ module.exports = {
                     fs.mkdirSync(currentDir)
                 }
             } catch (e) {
-                console.error("\x1b[31m", "\u00D7", "\x1b[0m", `Generate error: ${e.message}`);
+                console.error(
+                    "\x1b[31m", "\u00D7", "\x1b[0m", `Generate error: ${e.message}`
+                );
                 return false;
             }
         }
         return true;
     },
 
+    /**
+     * @param {string} filename 
+     */
     className(filename) {
         let className = "";
         filename.split("/").pop().split("-").forEach(
