bd10ae2d350cf06b185f9f5c028249ddde7c3b44
seeren <consultant@seeren.fr>
2018-12-26 18:10:05 +0100

    Add router service

---
+++ src/core/router.service.js
@@ -0,0 +1,44 @@
+import { Service } from "./service";
+
+export const RouterService = new class RouterService extends Service {
+
+    /**
+     * @constructor
+     */
+    constructor() {
+        super();
+        this.state = {};
+    }
+
+    /**
+     * @returns {Object}
+     */
+    get() {
+        return this.state;
+    }
+
+    /**
+     * @param {RouterComponent} router
+     * @param {Object} route
+     * @param {Object} param
+     * @param {boolean} push 
+     */
+    put(router, route, param, push) {
+        let path = route.path;
+        this.state.name = route.name;
+        this.state.param = param || {}
+        for (let prop in this.state.param) {
+            path = path.replace(":" + prop, this.state.param[prop])
+        }
+        if (route.component instanceof window.Function) {
+            route.component = new route.component;
+        }
+        window.history[
+            (push ? "pushState" : "replaceState")
+        ](this.state, route.name, path);
+        router.attach(route.component);
+        this.notify();
+    }
+
+
+}
