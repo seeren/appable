c21aad48766fb41dd8a1e05a3fa036e2005658bf
seeren <consultant@seeren.fr>
2018-12-31 15:33:18 +0100

    Turn template optionnal

--- src/components/component.js
+++ src/components/component.js
@@ -11,9 +11,9 @@ export class Component {
         this.row = 0;
         this.components = [];
         this.selector = options.selector;
-        this.template = options.template;
+        this.template = options.template || "";
         if (options.components) {
-            options.components.forEach(component => this.attach(component, true));
+            options.components.forEach((component) => this.attach(component, true));
         }
         window.document.createElement(options.selector);
     }
@@ -25,7 +25,7 @@ export class Component {
         if (this[hook]) {
             this[hook]()
         }
-        this.components.forEach(component => component.lifeCycle(hook));
+        this.components.forEach((component) => component.lifeCycle(hook));
     }
 
     /**
@@ -43,13 +43,10 @@ export class Component {
         this.components.push(component);
         this.row++;
         if (replace) {
-            this.template = this.template.replace(
-                `<${selector}>${endTag}`,
-                container
-            );
+            this.template = this.template.replace(`<${selector}>${endTag}`, container);
         } else {
             this.template += container;
-            this.lifeCycle("onInit")
+            this.lifeCycle("onInit");
         }
         return this;
     }
@@ -60,7 +57,7 @@ export class Component {
      */
     detach(component) {
         const index = this.components.indexOf(component);
-        if (index > -1) {
+        if (-1 < index) {
             const selectorSplit = component.selector.split("[");
             const selector = selectorSplit[0];
             const attributes = ` ${selectorSplit[1].replace("]", "")}`;
@@ -82,7 +79,7 @@ export class Component {
         const properties = window.Object.getOwnPropertyNames(this).slice(4).concat(
             window.Object.getOwnPropertyNames(this.constructor.prototype).slice(1)
         );
-        properties.forEach(varName => {
+        properties.forEach((varName) => {
             vars += `var ${varName} = this["${varName}"]`;
             if (this[varName] instanceof window.Function) {
                 vars += ".bind(this)";
@@ -100,21 +97,17 @@ export class Component {
 
     /**
      * @param {HTMLElement} htmlElement 
-     * @param {Array} properties 
+     * @param {String[]} properties 
      */
     updateEvents(htmlElement, properties) {
         let match;
-        properties.forEach(propertie => {
+        properties.forEach((propertie) => {
             const regExp = new window.RegExp(
                 `(on[a-zA-Z]{4,16})="${propertie}\\((\.*)\\)"`, "g"
             );
             while (match = regExp.exec(htmlElement.innerHTML)) {
-                window.document.querySelectorAll(
-                    `${this.selector} [${match[0]}]`
-                ).forEach(
-                    child => this.registerEvent(
-                        child, match[1], propertie, match[2].split(", ")
-                    )
+                window.document.querySelectorAll(`${this.selector} [${match[0]}]`).forEach(
+                    (child) => this.registerEvent(child, match[1], propertie, match[2].split(", "))
                 );
             }
         });
@@ -124,7 +117,7 @@ export class Component {
      * @param {HTMLElement} htmlElement 
      * @param {string} type 
      * @param {string} propertie 
-     * @param {Array} args 
+     * @param {String[]} args 
      */
     registerEvent(htmlElement, type, propertie, args) {
         htmlElement[type] = () => {
