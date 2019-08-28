183988cc8abea01ae93c8287ff8c8d0ad455500d
seeren <consultant@seeren.fr>
2019-08-28 12:38:56 +0200

    Import window

--- cli/skeleton/test/unit/app-component.spec.js
+++ cli/skeleton/test/unit/app-component.spec.js
@@ -1,5 +1,6 @@
 import { describe, beforeEach, it } from "mocha";
 import { assert } from "chai";
+import { window } from "./../window";
 import { AppComponent } from "../../src/app/app.component";
 
 describe("AppComponent", () => {
