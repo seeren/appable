28693b5c602d634e1e1743fafd89dac7bbeb45c6
seeren <consultant@seeren.fr>
2020-03-16 12:34:41 +0100

    Enforce documentation

--- src/models/route.model.js
+++ src/models/route.model.js
@@ -1,16 +1,32 @@
+import { Component } from "../components/component";
+
 /**
  * @type {Route}
  */
 export class Route {
 
     /**
-     * @param {string} path 
-     * @param {string} name 
+     * @constructor
+     * 
+     * @param {String} path 
+     * @param {String} name 
      * @param {Component} component 
      */
     constructor(path, name, component) {
+
+        /**
+         * @param {String} path 
+         */
         this.name = name;
+
+        /**
+         * @param {String} path 
+         */
         this.path = path;
+
+        /**
+         * @param {Component} path 
+         */
         this.component = component;
     }
 
