8bcccdcb12bc6e222363f2b4a6850d6a97684ce1
seeren <consultant@seeren.fr>
2020-03-17 16:39:36 +0100

    Add hook implementation

--- cli/template/component.js
+++ cli/template/component.js
@@ -1,14 +1,57 @@
 module.exports = (filename, className) => `import { Component } from "babel-skeleton";
-import { template } from "./${filename}.component.html";
-    
-export class ${className}Component extends Component {
 
+import { template } from "./${filename}.component.html";
+
+
+/**
+ * @type { ${className}Component}
+ */
+export class  ${className}Component extends Component {
+
+    /**
+     * @constructor
+     */
     constructor() {
         super({
             selector: "${filename}",
             template: template
         });
     }
+
+    /**
+     * Called after the component is attached
+     */
+    onInit() { }
+
+    /**
+     * Called after the component is displayed
+     * 
+     * @param {HTMLElement}
+     */
+    onUpdate(element) { }
+
+    /**
+     * Called after the component is detached
+     */
+    onDestroy() { }
+
+    /**
+     * Called after the user trigger the back button
+     * You can cancel the back navigation by returning false
+     * 
+     * @returns {Boolean}
+     */
+    onBack() { }
+
+    /**
+     * Called after the user trigger the pause button
+     */
+    onPause() { }
+
+    /**
+     * Called after the user trigger the resume button
+     */
+    onResume() { }
 
 }
 `;
