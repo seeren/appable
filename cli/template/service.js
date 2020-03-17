b5ca4255f05f851b9d9f9b1a70abf6dddb23401e
seeren <consultant@seeren.fr>
2020-03-17 16:39:48 +0100

    Fix CS

--- cli/template/service.js
+++ cli/template/service.js
@@ -1,14 +1,17 @@
-module.exports = (className) => {
+module.exports = (className) => `import { Service } from 'babel-skeleton';
 
-    return `import { Service } from 'babel-skeleton';
 
+/**
+ * @type {${className}Service}
+ */
 export const ${className}Service = new class extends Service {
 
+    /**
+     * @constructor
+     */
     constructor() {
         super();
     }
 
 }
 `;
-
-}
