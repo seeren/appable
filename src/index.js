26c0a75f77742ac871a7ac717f5961df608a2f98
seeren <consultant@seeren.fr>
2020-03-17 17:41:40 +0100

    Remove router service export

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 export { Component } from "./components/component";
 export { RouterComponent } from "./components/router.component";
 export { Service } from "./services/service";
-export { RouterService } from "./services/router.service";
+
