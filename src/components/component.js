2fbe7076abb55610fc07bf4a16b4d9fa073e3493
seeren <consultant@seeren.fr>
2020-03-17 14:49:19 +0100

    Add life cycle

--- src/components/component.js
+++ src/components/component.js
@@ -38,12 +38,33 @@ export class Component {
     }
 
     /**
-     * @param {Component} component 
+     * @param {String} hookName 
+     * @returns {Boolean}
+     */
+    lifeCycle(hookName) {
+        let hookValue = null;
+        if (this[hookName]) {
+            hookValue = this[hookName]();
+        }
+        this.components.forEach((component) => {
+            if (hookValue === false) {
+                return component.lifeCycle(hookName);
+            }
+            hookValue = component.lifeCycle(hookName);
+        });
+        return hookValue;
+    }
+
+    /**
+     * @param {Component|Function} component 
      * @returns {Component}
      * 
      * @throws {ReferenceError}
      */
     attach(component) {
+        if (component instanceof window.Function) {
+            component = new component;
+        }
         const selector = component.selector.split("[")[0];
         if (-1 !== this.components.indexOf(component)) {
             throw new ReferenceError(`Can't attach "${selector}": instance already exist`);
@@ -74,12 +95,13 @@ export class Component {
      */
     detach(component) {
         const index = this.components.indexOf(component);
-        const selectorSplit = component.selector.split("[");
-        const selector = selectorSplit[0];
-        if (-1 === index || 1 === selectorSplit.length) {
-            throw new ReferenceError(`Can't detach "${selector}": make sure you attach it before`);
+        const selectorSplitHook = component.selector.split("[");
+        if (-1 === index || 1 === selectorSplitHook.length) {
+            throw new ReferenceError(`Can't detach "${component.selector}": make sure you attach it before`);
         }
-        const attributes = `${selectorSplit[1].replace("]", "")}`;
+        const selectorSplitSpace = selectorSplitHook[selectorSplitHook.length - 2].split(" ");
+        const selector = selectorSplitSpace[selectorSplitSpace.length - 1];
+        const attributes = `${selectorSplitHook[selectorSplitHook.length - 1].replace("]", "")}`;
         const match = new RegExp(`<${selector}+(.)+${attributes}+><`).exec(this.template);
         this.template = this.template.replace(`${match[0]}/${selector}>`, "");
         this.components.splice(index, 1);
@@ -113,23 +135,16 @@ export class Component {
             vars += ";";
         });
         element.innerHTML = eval(vars + '`' + this.template + '`');
-        this.updateEvents(element, properties).updateComponents();
-        if (this.onUpdate) {
-            this.onUpdate(element)
-        }
-        return this;
-    }
-
-    /**
-     * @returns {Component}
-     */
-    updateComponents() {
+        this.updateEvents(element, properties);
         this.components.forEach((component) => {
             if (-1 === component.selector.indexOf(this.selector)) {
                 component.selector = `${this.selector} ${component.selector}`;
             }
             component.update();
         });
+        if (this.onUpdate) {
+            this.onUpdate(element)
+        }
         return this;
     }
 
