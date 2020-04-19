879ddfa49d3d36b8a6514934cedcfd598b3ea1cf
seeren <consultant@seeren.fr>
2020-04-19 20:10:34 +0200

    Use double quote

--- cli/skeleton/webpack.config.js
+++ cli/skeleton/webpack.config.js
@@ -1,41 +1,41 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
 
 module.exports = {
     entry: [
-        './src/index.js',
-        './src/index.scss'
+        "./src/index.js",
+        "./src/index.scss"
     ],
     output: {
         path: __dirname + "/www/dist",
-        filename: 'index.js'
+        filename: "index.js"
     },
     module: {
         rules: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: 'babel-loader'
+                loader: "babel-loader"
             },
             {
                 test: /\.html$/,
                 exclude: /node_modules/,
-                use: 'raw-loader',
+                use: "raw-loader",
             },
             {
                 test: /\.scss$/,
                 exclude: /node_modules/,
                 use: [
                     MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader'
+                    "css-loader",
+                    "sass-loader"
                 ]
             },
             {
                 test: /\.(jpg|png|woff|woff2|eot|ttf|svg)$/,
-                loader: 'file-loader',
+                loader: "file-loader",
                 options: {
-                    name: '[path][name].[ext]'
+                    name: "[path][name].[ext]"
                 },
             }
         ]
@@ -53,15 +53,15 @@ module.exports = {
         ]
     },
     plugins: [
-        new MiniCssExtractPlugin({ filename: 'index.css', }),
+        new MiniCssExtractPlugin({ filename: "index.css", }),
         new BrowserSyncPlugin({
-            host: 'localhost',
+            host: "localhost",
             port: 3000,
-            files: ['www/index.html',],
+            files: ["www/index.html",],
             server: {
-                baseDir: 'www',
+                baseDir: "www",
                 middleware: (req, res, next) => -1 === req.url.indexOf(".") && "/" !== req.url
-                    ? res.end(res.writeHead(302, { 'Location': '/' }))
+                    ? res.end(res.writeHead(302, { "Location": "/" }))
                     : next()
             }
         })
