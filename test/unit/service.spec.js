ab1b3a6d807e6fd5c1ea91bab98df9f1aa2cd1b0
seeren <consultant@seeren.fr>
2020-04-23 19:09:03 +0200

    Lint

--- test/unit/service.spec.js
+++ test/unit/service.spec.js
@@ -1,27 +1,27 @@
-import { describe, beforeEach, it } from "mocha";
-import { assert } from "chai";
-import { Service } from "./../../src/services/service";
+import { describe, beforeEach, it } from 'mocha';
+import { assert } from 'chai';
+import { Service } from '../../src/services/service';
 
-describe("Service", () => {
+describe('Service', () => {
 
     let service;
 
-    beforeEach(() => service = new Service);
+    beforeEach(() => { service = new Service(); });
 
-    describe("notify", () => {
-        it("call attached callback", () => {
+    describe('notify', () => {
+        it('call attached callback', () => {
             let called = false;
-            let callable = () => called = true;
+            const callable = () => { called = true; };
             service.attach(callable);
             service.notify();
             assert.isTrue(called);
         });
     });
 
-    describe("attach", () => {
-        it("Allow collection", () => {
+    describe('attach', () => {
+        it('Allow collection', () => {
             let called = 0;
-            let callable = () => called++;
+            const callable = () => { called += 1; };
             service.attach(callable);
             service.attach(callable);
             service.notify();
@@ -29,10 +29,10 @@ describe("Service", () => {
         });
     });
 
-    describe("detach", () => {
-        it("Remove attached", () => {
+    describe('detach', () => {
+        it('Remove attached', () => {
             let called = false;
-            let callable = () => called = true;
+            const callable = () => { called = true; };
             service.attach(callable);
             service.detach(callable);
             service.notify();
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,20 @@
 module.exports = {
-    entry: "./src/index.js",
+    entry: './src/index.js',
     output: {
-        path: __dirname + "/dist",
-        filename: "index.js",
-        libraryTarget: "umd"
+        path: `${__dirname}/dist`,
+        filename: 'index.js',
+        libraryTarget: 'umd',
     },
     module: {
         rules: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                use: "babel-loader"
-            }
-        ]
+                use: 'babel-loader',
+            },
+        ],
     },
     watchOptions: {
-        ignored: /node_modules/
-    }
+        ignored: /node_modules/,
+    },
 };
