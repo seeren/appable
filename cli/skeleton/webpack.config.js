4b28d5a1b1af03e57edc629c8b5ae1085d8fa728
seeren <consultant@seeren.fr>
2020-04-23 17:02:07 +0200

    Lint

--- cli/skeleton/webpack.config.js
+++ cli/skeleton/webpack.config.js
@@ -1,44 +1,44 @@
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 module.exports = {
     entry: [
-        "./src/index.js",
-        "./src/index.scss"
+        './src/index.js',
+        './src/index.scss',
     ],
     output: {
-        path: __dirname + "/www/dist",
-        filename: "index.js"
+        path: `${__dirname}/www/dist`,
+        filename: 'index.js',
     },
     module: {
         rules: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: "babel-loader"
+                loader: 'babel-loader',
             },
             {
                 test: /\.html$/,
                 exclude: /node_modules/,
-                use: "raw-loader",
+                use: 'raw-loader',
             },
             {
                 test: /\.scss$/,
                 exclude: /node_modules/,
                 use: [
                     MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    "sass-loader"
-                ]
+                    'css-loader',
+                    'sass-loader',
+                ],
             },
             {
                 test: /\.(jpg|png|woff|woff2|eot|ttf|svg)$/,
-                loader: "file-loader",
+                loader: 'file-loader',
                 options: {
-                    name: "[path][name].[ext]"
+                    name: '[path][name].[ext]',
                 },
-            }
-        ]
+            },
+        ],
     },
     watchOptions: {
         ignored: [
@@ -49,21 +49,21 @@ module.exports = {
             /plugins/,
             /resources/,
             /test/,
-            /www/
-        ]
+            /www/,
+        ],
     },
     plugins: [
-        new MiniCssExtractPlugin({ filename: "index.css", }),
+        new MiniCssExtractPlugin({ filename: 'index.css' }),
         new BrowserSyncPlugin({
-            host: "localhost",
+            host: 'localhost',
             port: 3000,
-            files: ["www/index.html",],
+            files: ['www/index.html'],
             server: {
-                baseDir: "www",
-                middleware: (req, res, next) => -1 === req.url.indexOf(".") && "/" !== req.url
-                    ? res.end(res.writeHead(302, { "Location": "/" }))
-                    : next()
-            }
-        })
-    ]
+                baseDir: 'www',
+                middleware: (req, res, next) => (-1 === req.url.indexOf('.') && '/' !== req.url
+                    ? res.end(res.writeHead(302, { Location: '/' }))
+                    : next()),
+            },
+        }),
+    ],
 };
